fd9c0fa86bc8ff0fae99ab9b624940a18ffc7d66
Jessy Williams <jessy@jessywilliams.com>
2020-07-23 23:17:57 -0400

    Fixes volume_str formatting

--- stonky/stock.py
+++ stonky/stock.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from decimal import Decimal
 
 from stonky.forex import Forex
 
@@ -6,7 +7,7 @@ from stonky.forex import Forex
 @dataclass
 class Stock:
     ticket: str = ""
-    currency_code: str = ""
+    currency_code: str = "USD"
     amount_bid: float = 0
     amount_ask: float = 0
     amount_low: float = 0
@@ -14,28 +15,23 @@ class Stock:
     amount_prev_close: float = 0
     delta_amount: float = 0
     delta_percent: float = 0
-    volume: int = 0
+    volume: float = 0
 
     def __post_init__(self):
         self.ticket = self.ticket.upper()
         self.currency_code = self.currency_code.upper()
 
     @property
-    def delta_percent_str(self) -> str:
-        s = f"{self.delta_percent*100:+.2f}%"
-        return s
-
-    @property
     def volume_str(self) -> str:
         if self.volume >= 1_000_000_000:
-            s = f"{self.volume/1_000_00_000:.1f}"
-            s = s.rstrip(".0") + "B"
+            d = Decimal(self.volume / 1_000_000_000).quantize(Decimal(".1"), rounding="ROUND_DOWN")
+            s = str(d).rstrip(".0") + "B"
         elif self.volume >= 1_000_000:
-            s = f"{self.volume/1_000_00:.1f}"
-            s = s.rstrip(".0") + "M"
+            d = Decimal(self.volume / 1_000_000).quantize(Decimal(".1"), rounding="ROUND_DOWN")
+            s = str(d).rstrip(".0") + "M"
         elif self.volume >= 1_000:
-            s = f"{self.volume/1_000:.1f}"
-            s = s.rstrip(".0") + "K"
+            d = Decimal(self.volume / 1_000).quantize(Decimal(".1"), rounding="ROUND_DOWN")
+            s = str(d).rstrip(".0") + "K"
         else:
             s = f"{self.volume:.2f}"
         return s
@@ -61,7 +57,7 @@ class Stock:
         s += self.volume_str.ljust(8)
         s += "@ " + f"{self.amount_bid:.2f}".ljust(8)
         s += symbol
-        s += f"{self.delta_amount:+,.2f} {self.delta_percent*100:+.2f}%".rjust(14)
+        s += f"{self.delta_amount:+,.2f} {self.delta_percent*100:+.2f}%".rjust(15)
         return s
 
     @property
@@ -85,4 +81,3 @@ class Stock:
         self.amount_low *= rate
         self.amount_high *= rate
         self.amount_prev_close *= rate
-        self.delta_amount *= rate
