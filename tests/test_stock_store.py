3835b41fafc094adde7f7bd0acaa7aa195ac6c34
Jessy Williams <jessy@jessywilliams.com>
2020-08-13 21:11:15 -0400

    Compares using ticket strings instead of stock objects for easier diff readability

--- tests/test_stock_store.py
+++ tests/test_stock_store.py
@@ -1,3 +1,4 @@
+from copy import copy
 from unittest import IsolatedAsyncioTestCase
 from unittest.mock import AsyncMock
 
@@ -12,13 +13,10 @@ class TestStockStore(IsolatedAsyncioTestCase):
     async def asyncSetUp(self) -> None:
         self.api = Api()
         self.api._query = AsyncMock()
-        self.settings = Settings()
-
-
-class TestWatchlist(TestStockStore):
-    async def asyncSetUp(self) -> None:
-        await super().asyncSetUp()
-        self.settings.watchlist = {"AAPL", "AMD", "SHOP.TO", "VGRO.TO"}
+        self.settings = Settings(
+            positions={"AAPL": 1, "AMD": 10, "SHOP.TO": 100, "VGRO.TO": 1000,},
+            watchlist=["AAPL", "AMD", "SHOP.TO", "VGRO.TO"],
+        )
 
     @property
     def stock_store(self):
@@ -32,128 +30,134 @@ class TestWatchlist(TestStockStore):
         }
         return stock_store
 
+
+class TestWatchlist(TestStockStore):
+    @property
+    def watch_tickets(self):
+        return [stock.ticket for stock in self.stock_store.watchlist]
+
     async def test_sort_amount(self):
         self.settings.sort = SortType.AMOUNT
-        assert self.stock_store.watchlist == [
-            VGRO_TO_STOCK,
-            AAPL_STOCK,
-            AMD_STOCK,
-            SHOP_TO_STOCK,
+        assert self.watch_tickets == [
+            "VGRO.TO",
+            "AAPL",
+            "AMD",
+            "SHOP.TO",
         ]
 
     async def test_sort_amount_desc(self):
         self.settings.sort = SortType.AMOUNT_DESC
-        assert self.stock_store.watchlist == [
-            SHOP_TO_STOCK,
-            AMD_STOCK,
-            AAPL_STOCK,
-            VGRO_TO_STOCK,
+        assert self.watch_tickets == [
+            "SHOP.TO",
+            "AMD",
+            "AAPL",
+            "VGRO.TO",
         ]
 
     async def test_sort_ticket(self):
         self.settings.sort = SortType.TICKET
-        assert self.stock_store.watchlist == [
-            AAPL_STOCK,
-            AMD_STOCK,
-            SHOP_TO_STOCK,
-            VGRO_TO_STOCK,
+        assert self.watch_tickets == [
+            "AAPL",
+            "AMD",
+            "SHOP.TO",
+            "VGRO.TO",
         ]
 
     async def test_sort_ticket_desc(self):
         self.settings.sort = SortType.TICKET_DESC
-        assert self.stock_store.watchlist == [
-            VGRO_TO_STOCK,
-            SHOP_TO_STOCK,
-            AMD_STOCK,
-            AAPL_STOCK,
+        assert self.watch_tickets == [
+            "VGRO.TO",
+            "SHOP.TO",
+            "AMD",
+            "AAPL",
         ]
 
     async def test_sort_low(self):
         self.settings.sort = SortType.LOW
-        assert self.stock_store.watchlist == [
-            VGRO_TO_STOCK,
-            AAPL_STOCK,
-            AMD_STOCK,
-            SHOP_TO_STOCK,
+        assert self.watch_tickets == [
+            "VGRO.TO",
+            "AAPL",
+            "AMD",
+            "SHOP.TO",
         ]
 
     async def test_sort_low_desc(self):
         self.settings.sort = SortType.LOW_DESC
-        assert self.stock_store.watchlist == [
-            SHOP_TO_STOCK,
-            AMD_STOCK,
-            AAPL_STOCK,
-            VGRO_TO_STOCK,
+        assert self.watch_tickets == [
+            "SHOP.TO",
+            "AMD",
+            "AAPL",
+            "VGRO.TO",
         ]
 
     async def test_sort_high(self):
         self.settings.sort = SortType.HIGH
-        assert self.stock_store.watchlist == [
-            VGRO_TO_STOCK,
-            AAPL_STOCK,
-            AMD_STOCK,
-            SHOP_TO_STOCK,
+        assert self.watch_tickets == [
+            "VGRO.TO",
+            "AAPL",
+            "AMD",
+            "SHOP.TO",
         ]
 
     async def test_sort_high_desc(self):
         self.settings.sort = SortType.HIGH_DESC
-        assert self.stock_store.watchlist == [
-            SHOP_TO_STOCK,
-            AMD_STOCK,
-            AAPL_STOCK,
-            VGRO_TO_STOCK,
+        assert self.watch_tickets == [
+            "SHOP.TO",
+            "AMD",
+            "AAPL",
+            "VGRO.TO",
         ]
 
     async def test_sort_close(self):
         self.settings.sort = SortType.CLOSE
-        assert self.stock_store.watchlist == [
-            VGRO_TO_STOCK,
-            AAPL_STOCK,
-            AMD_STOCK,
-            SHOP_TO_STOCK,
+        assert self.watch_tickets == [
+            "VGRO.TO",
+            "AAPL",
+            "AMD",
+            "SHOP.TO",
         ]
 
     async def test_sort_close_desc(self):
         self.settings.sort = SortType.CLOSE_DESC
-        assert self.stock_store.watchlist == [
-            SHOP_TO_STOCK,
-            AMD_STOCK,
-            AAPL_STOCK,
-            VGRO_TO_STOCK,
+        assert self.watch_tickets == [
+            "SHOP.TO",
+            "AMD",
+            "AAPL",
+            "VGRO.TO",
         ]
 
     async def test_sort_change(self):
         self.settings.sort = SortType.CHANGE
-        assert self.stock_store.watchlist == [
-            AAPL_STOCK,
-            VGRO_TO_STOCK,
-            SHOP_TO_STOCK,
-            AMD_STOCK,
+        assert self.watch_tickets == [
+            "AAPL",
+            "VGRO.TO",
+            "SHOP.TO",
+            "AMD",
         ]
 
     async def test_sort_change_desc(self):
         self.settings.sort = SortType.CHANGE_DESC
-        assert self.stock_store.watchlist == [
-            AMD_STOCK,
-            SHOP_TO_STOCK,
-            VGRO_TO_STOCK,
-            AAPL_STOCK,
+        assert self.watch_tickets == [
+            "AMD",
+            "SHOP.TO",
+            "VGRO.TO",
+            "AAPL",
         ]
 
     async def test_sort_volume(self):
         self.settings.sort = SortType.VOLUME
-        assert self.stock_store.watchlist == [
-            VGRO_TO_STOCK,
-            SHOP_TO_STOCK,
-            AMD_STOCK,
-            AAPL_STOCK,
+        assert self.watch_tickets == [
+            "VGRO.TO",
+            "SHOP.TO",
+            "AMD",
+            "AAPL",
         ]
 
     async def test_sort_volume_desc(self):
         self.settings.sort = SortType.VOLUME_DESC
-        assert self.stock_store.watchlist == [
-            AAPL_STOCK,
-            AMD_STOCK,
-            SHOP_TO_STOCK,
-            VGRO_TO_STOCK,
+        assert self.watch_tickets == [
+            "AAPL",
+            "AMD",
+            "SHOP.TO",
+            "VGRO.TO",
         ]
